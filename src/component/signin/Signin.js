6d33df42a11e4746c50df7716279e8525affc410
Harsha-S2603 <harsha.s@customerlabs.co>
2021-06-27 15:39:34 +0530

    change in sign_in

--- src/component/signin/Signin.js
+++ src/component/signin/Signin.js
@@ -79,58 +79,65 @@ export default class Signin extends Component {
         const isEnabled = (this.state.passwordErrorMessage === "" && this.state.emailErrorMessage === "" && this.state.password !== "" && this.state.email !== "")
         return (
             <div>
-                <div className="container">
-                    <div className="card-config">
-                        <div className="card shadow mx-auto card-width">
-                            <div className="card-body">
-                                <h2 className="primary-color text-center card-title pt-4">Welcome to GENZ blog</h2><br/>
+                <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#signInModalCenter">Signin</button>
+                <div className="modal fade" id="signInModalCenter" tabIndex="-1" role="dialog" aria-labelledby="signInModalCenter" aria-hidden="true">
+                    <div className="modal-dialog modal-dialog-centered" role="document">
+                        <div className="modal-content"> 
+                            <div className="modal-body">
                                 <div className="container">
-                                    <div>
-                                        <button type="button" className="btn btn-light col-12"><FcGoogle />&nbsp; Sign in with Google</button><br/><br/>
-                                        <button type="button" className="btn btn-light col-12"><AiFillFacebook />&nbsp; Sign in with Facebook</button><br/><br/>
-                                    </div><br/>
-                                    <hr className="sep-login-hr" data-content="Have password? continue with your email address" /><br/>
-                                    <form>
-                                        <div className="form-group">
-                                            <label  className="pb-3" htmlFor="email">Email</label>
-                                            <input  type="email"
-                                                    name="email"
-                                                    required
-                                                    className="form-control"
-                                                    value={this.state.email}
-                                                    onChange={this.handleChange}
-                                                    id="email" aria-describedby="emailHelp" placeholder="Enter email" />
-                                            {this.state.emailErrorMessage !== "" ? <p className="text-danger pt-2">{this.state.emailErrorMessage}</p> : null}
+                                    <button type="button" className="close remove-button-css" data-dismiss="modal" aria-label="Close">
+                                        <span className="font-size-25" aria-hidden="true">&times;</span>
+                                    </button>
+                                    <h2 className="primary-color text-center card-title pt-4">Welcome to GenZ blog</h2><br/>
+                                    <div className="container">
+                                        <div>
+                                            <button type="button" className="btn btn-light col-12"><FcGoogle />&nbsp; Sign in with Google</button><br/><br/>
+                                            <button type="button" className="btn btn-light col-12"><AiFillFacebook />&nbsp; Sign in with Facebook</button><br/><br/>
                                         </div><br/>
-                                        <div className="form-group">
-                                            <label  className="pb-3" htmlFor="password">Password</label>
-                                            <input  type="password" 
-                                                    name="password"
-                                                    required
-                                                    className="form-control" 
-                                                    value={this.state.password}
-                                                    onChange={this.handleChange}
-                                                    id="password" placeholder="Enter password" />
-                                            {this.state.passwordErrorMessage !== "" ? <p className="text-danger pt-2">{this.state.passwordErrorMessage}</p> : null}
-                                                    
-                                        </div><br/>
-                                        <div className="form-group">
-                                            <input type="checkbox" className="switch" id="show password" 
-                                            name="show password" onChange={this.handleChange} 
-                                            value="Show Password" />&nbsp;
-                                            <label  className="pb-3" htmlFor="show password">Show password</label>
-                                        </div>
-                                        
-                                        <div className="form-group">
-                                            <button type="button" onClick={this.handleLogin} className="btn-config btn-primary-col" disabled={!isEnabled}>Log in</button>
-                                        </div>
-                                    </form><br/>
-                                    <p>Don't have an account? <a className="primary-color" href="/register">Sign up</a></p>
+                                        <hr className="sep-login-hr" data-content="Have password? continue with your email address" /><br/>
+                                        <form>
+                                            <div className="form-group">
+                                                <label  className="pb-3" htmlFor="email">Email</label>
+                                                <input  type="email"
+                                                        name="email"
+                                                        required
+                                                        className="form-control"
+                                                        value={this.state.email}
+                                                        onChange={this.handleChange}
+                                                        id="email" aria-describedby="emailHelp" placeholder="Enter email" />
+                                                {this.state.emailErrorMessage !== "" ? <p className="text-danger pt-2">{this.state.emailErrorMessage}</p> : null}
+                                            </div><br/>
+                                            <div className="form-group">
+                                                <label  className="pb-3" htmlFor="password">Password</label>
+                                                <input  type="password" 
+                                                        name="password"
+                                                        required
+                                                        className="form-control" 
+                                                        value={this.state.password}
+                                                        onChange={this.handleChange}
+                                                        id="password" placeholder="Enter password" />
+                                                {this.state.passwordErrorMessage !== "" ? <p className="text-danger pt-2">{this.state.passwordErrorMessage}</p> : null}
+                                                        
+                                            </div><br/>
+                                            <div className="form-group">
+                                                <input type="checkbox" className="switch" id="show password" 
+                                                name="show password" onChange={this.handleChange} 
+                                                value="Show Password" />&nbsp;
+                                                <label  className="pb-3" htmlFor="show password">Show password</label>
+                                            </div>
+                                            
+                                            <div className="form-group">
+                                                <button type="button" onClick={this.handleLogin} className="btn-config btn-primary-col" disabled={!isEnabled}>Log in</button>
+                                            </div>
+                                        </form><br/>
+                                        <p>Don't have an account? <a className="primary-color" href="/register">Sign up</a></p>
+                                    </div>  
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
+import 'jquery/dist/jquery.min.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
