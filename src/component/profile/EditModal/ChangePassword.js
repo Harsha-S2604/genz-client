07d3b68c3b30820a3000f0c8b1654d6d2a88ab19
Harsha-S2604 <harsha91148@outlook.com>
2021-12-16 06:52:14 +0000

    second stage implementation of change password

--- src/component/profile/EditModal/ChangePassword.js
+++ src/component/profile/EditModal/ChangePassword.js
@@ -1,4 +1,8 @@
+import axios from 'axios';
 import React, {Component} from 'react';
+import { toast } from 'react-toastify';
+import { connect } from 'react-redux';
+
 
 class ChangePassword extends Component {
     constructor(props) {
@@ -11,6 +15,40 @@ class ChangePassword extends Component {
             newPasswdErr: "",
             confirmNewPasswdErr: ""
         }
+    }
+
+    changePassword = () => {
+        let passwordObj = {
+            userId: this.props.userProfileData["UserId"],
+            oldPasswd: this.state.oldPasswd,
+            newPasswd: this.state.newPasswd,
+            confirmPasswd: this.state.confirmNewPasswd
+        }
+        let reqConfig = {
+            headers: {
+                "X-Genz-Token": "4439EA5BDBA8B179722265789D029477",
+                'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
+            }
+        }
+        axios.post('http://localhost:8080/genz-server/user-api/change-passwd', passwordObj, reqConfig)
+            .then(response => {
+                if(response.data.success) {
+                    this.setState({
+                        oldPasswd: "",
+                        newPasswd: "",
+                        confirmNewPasswd: "",
+                        oldPasswdErr: "",
+                        newPasswdErr: "",
+                        confirmNewPasswdErr: ""  
+                    })
+                    toast.success(response.data.message);
+                } else {
+                    toast.error(response.data.message);
+                }
+            })
+            .catch((err) => {
+                toast.error("Sorry my friend, There's a problem from our side. We'll fix it ASAP. Please try again later.")
+            })
     }
 
     handlePasswordChange = (event) => {
@@ -36,7 +74,6 @@ class ChangePassword extends Component {
                 break;
             
             case "new_passwd":
-                var passwordPattern = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$");
                 if(value === "") {
                     this.setState({
                         newPasswdErr: "*Field Required",
@@ -47,24 +84,36 @@ class ChangePassword extends Component {
                         newPasswdErr: "*Password should be minimum of 6 characters",
                         newPasswd: value
                     })
-                } else {
-
-                    if(passwordPattern.test(value)) {
-                        this.setState({
-                            newPasswdErr: "",
-                            newPasswd: value
-                        })
-                    } else {
-                        this.setState({
-                            newPasswdErr: "*should contain atleast 1 uppercase letter, 1 lowercase letter, 1 numeric, 1 special character.",
-                            newPasswd: value
-                        })
-                    }
-
+                } else if(value === this.state.oldPasswd) {
+                    this.setState({
+                        newPasswdErr: "*New password cannot be same as old password",
+                        newPasswd: value
+                    })
+                } else { 
+                    this.setState({
+                        newPasswdErr: "",
+                        newPasswd: value
+                    })
                 }
                 break;
 
             case "confirm_new_passwd":
+                if(value === "") {
+                    this.setState({
+                        confirmNewPasswdErr: "*Field Required",
+                        confirmNewPasswd: value
+                    })
+                } else if(value !== this.state.newPasswd) {
+                    this.setState({
+                        confirmNewPasswd: value,
+                        confirmNewPasswdErr: "*Password do not match"
+                    })
+                } else {
+                    this.setState({
+                        confirmNewPasswd: value,
+                        confirmNewPasswdErr: ""
+                    })
+                }
                 break;
             
             default:
@@ -73,6 +122,7 @@ class ChangePassword extends Component {
         }
     }
     render() {
+        const isDisabled = !(this.state.confirmNewPasswdErr === "" && this.state.newPasswdErr === "" && this.state.oldPasswdErr === "" && this.state.oldPasswd !== "" && this.state.newPasswd !== "" && this.state.confirmNewPasswd !== "")
         return (
             <div className="modal fade" id="chng_passwd" tabIndex="-1" role="dialog" aria-labelledby="chng_passwd" aria-hidden="true">
                 <div className="modal-dialog modal-dialog-centered" role="document">
@@ -89,32 +139,50 @@ class ChangePassword extends Component {
                                             <label htmlFor="old_passwd"><b>Old Password</b></label>
                                             <input type="password"
                                                 required
-                                                className="form-control" 
+                                                value={this.state.oldPasswd}
+                                                className="form-control"
+                                                onChange={this.handlePasswordChange}
                                                 name="old_passwd"
                                                 id="old_passwd"
                                             />
+                                            {
+                                                this.state.oldPasswdErr ? 
+                                                <span className="error_message_css_config pt-2">{this.state.oldPasswdErr}</span>:null
+                                            }
                                         </div>
                                         <div className="form-group pb-3">
                                             <label htmlFor="new_passwd"><b>New Password</b></label>
                                             <input type="password"
                                                 required
+                                                onChange={this.handlePasswordChange}
+                                                value={this.state.newPasswd}
                                                 className="form-control" 
                                                 name="new_passwd"
                                                 id="new_passwd"
                                             />
+                                            {
+                                                this.state.newPasswdErr ? 
+                                                <span className="error_message_css_config pt-2">{this.state.newPasswdErr}</span>:null
+                                            }
                                         </div>
                                         <div className="form-group pb-3">
                                             <label htmlFor="confirm_new_passwd"><b>Confirm New Password</b></label>
                                             <input type="password"
                                                 required
+                                                onChange={this.handlePasswordChange}
+                                                value={this.state.confirmNewPasswd}
                                                 className="form-control" 
                                                 name="confirm_new_passwd"
                                                 id="confirm_new_passwd"
                                             />
+                                            {
+                                                this.state.confirmNewPasswdErr ? 
+                                                <span className="error_message_css_config pt-2">{this.state.confirmNewPasswdErr}</span>:null
+                                            }
                                         </div>
                                     </form>
                                     <div style={{float: "right"}}>
-                                        <button className="btn btn-outline-dark">Save</button>
+                                        <button className="btn btn-outline-dark" disabled={isDisabled} onClick={this.changePassword}>Save</button>
                                     </div>
                                 </div>
                             </div>
@@ -127,4 +195,14 @@ class ChangePassword extends Component {
     }
 }
 
-export default ChangePassword
+const mapStatetoProps = (state) => {
+    let { profileConfig } = state;
+    return { ...profileConfig }
+}
+
+const mapDispatchProps = (dispatch) => {
+    return {
+    }
+}
+
+export default connect(mapStatetoProps, mapDispatchProps)(ChangePassword)
